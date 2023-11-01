f2dbcdf348b85b3e63432e85587231cb77329f38
u46cm8 <touch2who@gmail.com>
2023-11-01 21:23:02 +0000

    The file is now complete several problems were encountered, one that persised was the div hieght getting the math right and accouting for the margins etc - grids added - flex was used and calibrated - added the listener and events to make the trail effect on the grid -  made the prompt to choose the matrix length and height - functions was added to remake the square matrics - problem solved the issue with the hieght spacing with regard to the flexgrow     to make sure the matrices drawn are square one     + NOTE +     this issue will arise again if the limit of 8 squares is exptended - limit of a length hieght of 8 square by screen was added.

--- ecma.js
+++ ecma.js
@@ -1,25 +1,58 @@
 var containerDiv = document.getElementById("flex-container");
+var n = 4;
+addGrids(n);
 
-for(var i=1; i <= 16; i++ ){
+
+
+function addGrids(n) {
+
+
+    for(var i=1; i <= n**2; i++ ){
     var newDiv = document.createElement("div");
-
+    
+    // count of squeres
     // newDiv.innerHTML = '<p>'+i+'</p>';
 
-    newDiv.id = '' +i;
     newDiv.className = 'grid';
     // newDiv.addEventListener('mousemove',e => {newDiv.style.background="blue"});
 
     containerDiv.appendChild(newDiv);
+    }
+    
+    document.body.addEventListener('mouseover', e => {
+        let d = e.target.closest('div');
+        if (!d || d.id=="flex-container"|| d.className=="buttons") { return; }
+        d.style.backgroundColor = 'khaki';
+      });
+      
+      document.body.addEventListener('mouseout', e => {
+        let d = e.target.closest('div');
+        if (!d || d.id=="flex-container") { return; }
+        setTimeout(() => {d.style.backgroundColor = '';},1000);
+      });
+      let gridList = document.getElementsByClassName("grid");
+      for (var y=0; y<= gridList.length; y++ ){
+        var grid = gridList[y];
+        var pageP = 100/(n) ;
+        grid.style.flex = (pageP-(1/n)*n)+"%",1,(pageP-(1/n)*n)+"%";
+         grid.style.height = 100/(n)  + "%";
+      }
+      // document.querySelector(".grid").style.height= "20%";
 }
 
-document.body.addEventListener('mouseover', e => {
-    let d = e.target.closest('div');
-    if (!d || d.id=="flex-container") { return; }
-    d.style.backgroundColor = 'khaki';
-  });
+//Test to see it worked
+//containerDiv.innerHTML="<h2>Something wrong :(</h2>";
+
+function squareChange() { 
+  n = prompt("Please enter the number of squares you would like on the sides \n only choose between 1 and 8", "");
+
+  if(n<9){
+    containerDiv.innerHTML="";
+    addGrids(n);
+  }
   
-  document.body.addEventListener('mouseout', e => {
-    let d = e.target.closest('div');
-    if (!d || d.id=="flex-container") { return; }
-    setTimeout(() => {d.style.backgroundColor = '';},1000);
-  });
+  else {
+    squareChange();
+  }
+
+}
